ed0ff7a62bcd1603fce6261fa8e4134b351bceb3
aiai-rs <js5553@outlook.com>
2025-12-13 23:11:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,11 +10,13 @@ self.addEventListener('push', (event) => {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
         body: data.body,
-        icon: '/icon-192.png',
-        badge: '/icon-192.png',
+        icon: '/icon-192.png',  // 通知的右侧大图（可以用彩色）
+        badge: '/badge.png',    // 【重要】状态栏小图标（必须是透明底+白色图案）
         data: { url: data.url || '/' },
         vibrate: [200, 100, 200],
-        requireInteraction: true 
+        tag: 'hy-msg',
+        renotify: true,
+        requireInteraction: true // 强制通知停留，直到用户点击
     });
 });
 
